9926a22e5ffb00a0b7d88cfeb9616e069258488e
Bryan Petrick <www.bryanpetrick@gmail.com>
2026-03-30 17:45:37 -0300

    teste

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-
-  <meta charset="UTF-8">
-  <title>Fundo espacial</title>
+  <title>Bem Vindo!</title>
   <link rel="stylesheet" href="/css/index.css">
-</head>
-<body>
-  <div class="space"></div>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bem Vindo!</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
