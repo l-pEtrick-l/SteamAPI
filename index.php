f6d08f0e541ad72c71d56c819df5b220c95147da
Bryan Petrick <www.bryanpetrick@gmail.com>
2026-03-30 17:56:38 -0300

    navbar

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-
-
- 
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bem Vindo!</title>
- <link rel="stylesheet" href="/css/index.css">
+  <meta charset="UTF-8">
+  <title>Fundo espacial</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="/css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-
   <div class="space"></div>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     
     <a class="navbar-brand" href="#">Navbar</a>
@@ -36,8 +31,10 @@
     </div>
   </div>
 </nav>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-
-
 </body>
 </html>
+
+
+
