a052094155a21ea1bb5c17b3e9c1ec6c8b0bd6bd
Bryan Petrick <www.bryanpetrick@gmail.com>
2026-04-01 14:01:31 -0300

    fundo

--- index.php
+++ index.php
@@ -44,7 +44,8 @@
 <head>
   <meta charset="UTF-8">
   <title>Fundo espacial</title>
-  <link rel="stylesheet" href="style.css">
+  
+  <link rel="stylesheet" href="/css/index.css">
 </head>
 <body>
   <div class="space"></div>
