d901d9e6dcbe808177abfd1eb30d8d2c9f9a35dd
Petrick <www.bryanpetrick@gmail.com>
2026-05-07 17:22:00 -0300

    verificacao de usuario existente, e adicao de coluna ativo

--- PHP/cadastro.php
+++ PHP/cadastro.php
@@ -12,13 +12,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $senha="teste";
     $checkbox=$_POST["checkbox"] ?? '';
     $data_nascimento = $_POST["data_nascimento"] ?? '';
-    $data_de_cadastro = date("Y-m-d h:i:s");
+    $data_de_cadastro = date("Y-m-d H:i:s");
+    $ativo = 1;
 
+    $select = $mysqli->prepare("SELECT email_usuario, telefone FROM user WHERE email_usuario = ? OR telefone = ?");
+    $select->bind_param("ss", $email, $telefone );
+    $select->execute();
+    $result = $select->get_result();
+    if($result->num_rows > 0){
+        echo json_encode(['ok' => false, 'message' => 'Email ou telefone já cadastrados']);
+        exit;
+    }
+
+    $stmt = $mysqli->prepare("SELECT nick FROM user WHERE nick = ?");
+    $stmt->bind_param("s", $nick);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result->num_rows > 0){
+        echo json_encode(['ok' => false, 'message' => 'Nick já cadastrado']);
+        exit;
+    }
     if(!empty($checkbox)){
         $senha_crip = password_hash($senha, PASSWORD_BCRYPT);
-        $sql = $mysqli->prepare("INSERT INTO user(nome, nick, telefone, email_usuario, data_nascimento, senha, data_de_cadastro)
-        VALUES (?,?,?,?,?,?,?)");
-        $sql-> bind_param("sssssss", $nome, $nick, $telefone, $email, $data_nascimento, $senha_crip, $data_de_cadastro);
+        $sql = $mysqli->prepare("INSERT INTO user(nome, nick, telefone, email_usuario, data_nascimento, senha, data_de_cadastro, ativo)
+        VALUES (?,?,?,?,?,?,?,?)");
+        $sql-> bind_param("sssssssi", $nome, $nick, $telefone, $email, $data_nascimento, $senha_crip, $data_de_cadastro, $ativo);
         $sql->execute();
     }else{
         echo json_encode(['ok' => false, 'message' => 'Necessário aceitar os termos de uso']);
