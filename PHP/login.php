329e6c711e10c4eac2b33561d40b2f7d873235c3
Petrick <www.bryanpetrick@gmail.com>
2026-05-07 10:03:56 -0300

    login backend finalizado

--- PHP/login.php
+++ PHP/login.php
@@ -4,20 +4,38 @@ $mysqli = include __DIR__ . '/../config/conectar.php';
 header('Content-Type: application/json; charset=UTF-8');
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $email = $_POST["email"] ?? null;
-    $senha = $_POST["senha"] ?? null;
+    $email = filter_var(trim($_POST['email'] ?? ''), FILTER_SANITIZE_EMAIL);
+    $senha = $_POST["senha"] ?? '';
 
-    $sql = $mysqli->prepare("SELECT id_usuario, senha, nome FROM user WHERE email_usuario = ?");
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo json_encode(['ok' => 'false', 'message' => 'Email Invalido']);
+        exit;
+    }
+    if(!empty($email) && !empty($senha)){
+    $sql = $mysqli->prepare("SELECT id_usuario, senha, nome, nick FROM user WHERE email_usuario = ?");
     $sql->bind_param("s", $email);
     $sql->execute();
     $result = $sql->get_result();
+
     if($result->num_rows == 0){
+        sleep(1);
         echo json_encode(['ok' => 'false', 'message' => 'Usuario ou senha incorretos']);
         exit;
     }
     $user = $result->fetch_assoc();
     if(password_verify($senha, $user['senha'])){
-        
+        $_SESSION['id_usuario'] = $user['id_usuario'];
+        $_SESSION['logado'] = true;
+        $_SESSION['email'] = $email;
+        $_SESSION['nome'] = $user['nome'];
+        $_SESSION['nick'] = $user['nick'];
+        header('Location: /../HTML/meus-jogos');
+        exit;
+    }else{
+        sleep(1);
+         echo json_encode(['ok' => 'false', 'message' => 'Usuario ou senha incorretos']);
+        exit;
+    }
     }
 }
 
